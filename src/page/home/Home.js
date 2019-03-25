bcab0741e2507db8211d19b7483f23e78b2b1b52
Calvin Thai <calvin@onlinebizsoft.com>
2019-03-25 15:59:46 +0700

    Structure project

---
+++ src/page/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './home.scss';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h4 className="homeTitle">Learen</h4>
+      </div>
+    );
+  }
+}
+
+export default Home;
